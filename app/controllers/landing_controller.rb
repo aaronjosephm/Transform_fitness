710066c96781eede2ff28b33c72bdd1993dfe45a
Aaron Mednick <aaronjosephm@gmail.com>
2018-09-12 15:31:27 -0400

    almost done with landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,7 @@
 class LandingController < ApplicationController
    def show
     @landingpage = ButterCMS::Page.get('*', 'landing').data.fields
+    @testimonialspage = ButterCMS::Page.get('*', 'testimonials').data.fields
 
   end
 end
