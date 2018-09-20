7580cfd0d2edef3fdd3e0fa8a0e0531480779297
Aaron Mednick <aaronjosephm@gmail.com>
2018-09-20 14:59:19 -0400

    fixed carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,47 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+// console.log('Hello World from Webpacker')
+
+// import 'bootstrap';
+
+// require('default-passive-events');
+
+// $(document).ready(function(){
+
+//   $("#myCarousel").carousel();
+
+//   console.log($("#myCarousel"));
+
+//    $(".item1").click(function(){
+//         $("#myCarousel").carousel(0);
+//     });
+//     $(".item2").click(function(){
+//         $("#myCarousel").carousel(1);
+//     });
+//     $(".item3").click(function(){
+//         $("#myCarousel").carousel(2);
+//     });
+
+//   $('#myCarousel').on('slide.bs.carousel', function () {
+//     console.log("new slide");
+//     const items = document.querySelectorAll('.carousel-item');
+//     let index = 0
+//     items.forEach(function(item, i) {
+//       console.log("entered loop");
+//       if (item.classList.contains("center-item")) {
+//         console.log(i);
+//         index = i;
+//         if (index === 2) {
+//           index = 0;
+//         }
+//         else {
+//           index = index + 1;
+//         }
+//         console.log(index);
+//         item.classList.remove("center-item");
+//       }
+//      });
+//     items[index].classList.add("center-item");
+//   });
+// });
