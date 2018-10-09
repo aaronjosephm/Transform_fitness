d3184164731a4e4f5edbbe7ad820b797b326bbd2
Aaron Mednick <aaronjosephm@gmail.com>
2018-10-09 12:34:41 -0700

    hope this wokrs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -35,7 +35,6 @@ class UserMailer < ApplicationMailer
 
   def form(email, question1, question2, question3, level, option, name)
     @user = {email: email, question1: question1, question2: question2, question3: question3, level: level, option: option, name: name}
-    admin = "f952f7f09ff9701beb911edec84127ab@inbound.postmarkapp.com"
     if (email.match(/\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i).nil? || name == "")
       return false
     else
@@ -45,12 +44,17 @@ class UserMailer < ApplicationMailer
         from: 'administrator@siphercatta.com',
         html_body: '<strong>Hello</strong> dear Postmark user.',
         track_opens: 'true')
-      mail(
+
+    end
+  end
+
+  def notify
+    admin = "f952f7f09ff9701beb911edec84127ab@inbound.postmarkapp.com"
+    mail(
         subject: 'You have a new client interested!',
         to: admin,
         from: 'administrator@siphercatta.com',
         html_body: '<strong>Hello</strong> dear Postmark user.',
         track_opens: 'true')
-    end
   end
 end
