6d85aa2568a151952078d8d5870d2b80f6e51523
Aaron Mednick <aaronjosephm@gmail.com>
2018-10-09 12:39:56 -0700

    changed it back

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -19,7 +19,7 @@ class ContactController < ApplicationController
     option = params[:gridRadios]
     email = params[:email]
     if UserMailer.form(email, question1, question2, question3, level, option, name).deliver_now
-      # UserMailer.notify.deliver_now
+      UserMailer.notify.deliver_now
       flash[:notice] = "Your form has been sent! We will get back to you shortly!"
       redirect_to contact_path
     else
