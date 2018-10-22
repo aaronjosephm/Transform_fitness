a6579670dd754107224de0f2167dde016e670c05
Aaron Mednick <aaronjosephm@gmail.com>
2018-10-22 16:43:05 -0700

    found bug in testimonials page

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -2,6 +2,6 @@ class TestimonialsController < ApplicationController
   def show
     @testimonialspage = ButterCMS::Page.get('*', 'testimonials').data.fields
     @images = [@testimonialspage.aaron_img, @testimonialspage.chris_img, @testimonialspage.tyler_img, @testimonialspage.client1_img, @testimonialspage.client2_img, @testimonialspage.client3_img]
-    @quotes = [@testimonialspage.chris_quote, @testimonialspage.aaron_quote, @testimonialspage.tyler_quote, @testimonialspage.client1_quote, @testimonialspage.client2_quote, @testimonialspage.client3_quote]
+    @quotes = [@testimonialspage.chris_quote, @testimonialspage.aaron_quote, @testimonialspage.tyler_text, @testimonialspage.client1_text, @testimonialspage.client2_text, @testimonialspage.client3_text]
   end
 end
