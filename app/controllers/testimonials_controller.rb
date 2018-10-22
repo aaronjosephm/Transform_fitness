ffd7f9fcd995b17684c480fc966090939815cd1f
Aaron Mednick <aaronjosephm@gmail.com>
2018-10-22 16:45:44 -0700

    changed order

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -2,6 +2,6 @@ class TestimonialsController < ApplicationController
   def show
     @testimonialspage = ButterCMS::Page.get('*', 'testimonials').data.fields
     @images = [@testimonialspage.aaron_img, @testimonialspage.chris_img, @testimonialspage.tyler_img, @testimonialspage.client1_img, @testimonialspage.client2_img, @testimonialspage.client3_img]
-    @quotes = [@testimonialspage.chris_quote, @testimonialspage.aaron_quote, @testimonialspage.tyler_text, @testimonialspage.client1_text, @testimonialspage.client2_text, @testimonialspage.client3_text]
+    @quotes = [@testimonialspage.aaron_quote, @testimonialspage.chris_quote, @testimonialspage.tyler_text, @testimonialspage.client1_text, @testimonialspage.client2_text, @testimonialspage.client3_text]
   end
 end
